705b7b70f905ae08948699688f9bbb27b50eeee9
Matheus <matheuswschneider@gmail.com>
2026-06-07 22:10:39 -0300

    Class Construtores

--- Program/src/entities/Product.java
+++ Program/src/entities/Product.java
@@ -5,6 +5,12 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product(String name, double price, int quantity) { // ISSO AQUI E UM CONSTRUTOR
+        this.name= name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public double totalValueInStock() {
         return price * quantity;
     }
